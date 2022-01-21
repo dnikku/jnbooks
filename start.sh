5906b121ab4f8a2cf0c8eced4fd08d485a0530d2
Nicolae Dascalu <n.dascalu@ceyoniq.com>
2022-01-22 00:39:08 +0100

    Revert "add jupyter vim-bindings extensions" (not working)
    This reverts commit 445283966e73901ebe615af9569766ebec8d08fd.

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-jupyter nbextension enable vim_binding/vim_binding
-
-jupyter lab --no-browser --allow-root --ip=0.0.0.0 --port=8888 --NotebookApp.token='' --NotebookApp.password='' --notebook-dir='/home/nbooks'
+jupyter-lab --no-browser --allow-root --ip=0.0.0.0 --port=8888 --NotebookApp.token='' --NotebookApp.password='' --notebook-dir='/home/nbooks'
